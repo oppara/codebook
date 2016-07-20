f2a31f982ad12f0fa4d6f22d4d40bd60f6ac9730
oppara <oppara@oppara.tv>
2016-07-21 02:36:54 +0900

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
-  match '/code_documents/search', :controller => 'code_documents', :action => 'search'
+  get '/code_documents/search', :controller => 'code_documents', :action => 'search'
   resources :code_documents
-  
+
   resources :code_languages, :only => [:create]
   resources :code_areas, :only => [:create]
-  
+
   resources :codebooks, :only => [:index] do
     collection do
-      match 'admin' 
-      match 'create_category'
-      match 'destroy_category'
-      match 'export'
-      match 'get_user'
-      match 'add_default_enums'      
+      match 'admin', :via => :all
+      match 'create_category', :via => :all
+      match 'destroy_category', :via => :all
+      match 'export', :via => :all
+      match 'get_user', :via => :all
+      match 'add_default_enums', :via => :all
     end
   end
-    
+
